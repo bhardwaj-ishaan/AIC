c35b1d107b379658d9ec22cd3196991b7e8ca880
bhardwaj-ishaan <bhardwaj.ishaan.codes@gmail.com>
2024-06-30 16:48:52 -0700

    added augment 2

---
+++ quiz/quiz.js
@@ -0,0 +1,7 @@
+$(document).ready(() => {
+    $('.input-group label, .input-group input, .input-group img').on('click', function() {
+        let inputGroup = $(this).closest('.input-group');
+        inputGroup.addClass('done');
+    }) 
+        
+})
