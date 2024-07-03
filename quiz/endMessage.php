4a9feda70c43b0410fa3ba4e07f87fca34f59965
bhardwaj-ishaan <bhardwaj.ishaan.codes@gmail.com>
2024-07-03 09:59:49 -0700

    added radio validation

--- quiz/endMessage.php
+++ quiz/endMessage.php
@@ -23,28 +23,33 @@
     } else{
         echo '<br>How much do you REALLY use reddit?';
     };
+    if(isset($_POST["room"]) && isset($_POST['clothingColor']) && isset($_POST['food']) && isset($_POST['organized']) && isset($_POST['pressure']) && isset($_POST['sick'])){
+        $social = htmlspecialchars($_POST["social"]);
+        $room = htmlspecialchars($_POST["room"]);
+        $clothing = htmlspecialchars($_POST["clothingColor"]);
+        $dinner = htmlspecialchars($_POST["food"]);
+        $organize = htmlspecialchars($_POST["organized"]);
+        $calm = htmlspecialchars($_POST["pressure"]);
+        $sick = htmlspecialchars($_POST["sick"]);
+    
+        echo  '<br>Your social level can be described as ' . $social . ' out of 10';
+    
+        echo '<br> Your favorite room type is ' . $room;
+    
+        echo '<br> You favorite clothing color is ' . $clothing;
+    
+        echo '<br> Your favorite dinner is ' . $dinner;
+    
+        echo '<br> You are ' . $organize . ' organized';
+    
+        echo '<br> You are ' . $calm . ' calm';
+    
+        echo '<br> You are sick ' . $sick;
+    } else{
+        echo 'One or more invalid inputs, go back and try again.';
+    }
 
 
 
-    $social = htmlspecialchars($_POST["social"]);
-    $room = htmlspecialchars($_POST["room"]);
-    $clothing = htmlspecialchars($_POST["clothingColor"]);
-    $dinner = htmlspecialchars($_POST["food"]);
-    $organize = htmlspecialchars($_POST["organized"]);
-    $calm = htmlspecialchars($_POST["pressure"]);
-    $sick = htmlspecialchars($_POST["sick"]);
 
-    echo  '<br>Your social level can be described as ' . $social . ' out of 10';
-
-    echo '<br> Your favorite room type is ' . $room;
-
-    echo '<br> You favorite clothing color is ' . $clothing;
-
-    echo '<br> Your favorite dinner is ' . $dinner;
-
-    echo '<br> You are ' . $organize . ' organized';
-
-    echo '<br> You are ' . $calm . ' calm';
-
-    echo '<br> You are sick ' . $sick;
 ?>
